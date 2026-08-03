1edc83560ac3fda8d1ec8d1616dc29ce0a8222b6
Avinash Kumar <avinashrk2107@gmail.com>
2026-08-03 18:48:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (10.38%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string ans = "";
-        int i = 0, j = 0;
+        string ans;
+        ans.reserve(word1.size() + word2.size());   // avoid reallocations
 
-        while (i < word1.size() && j < word2.size()) {
-            ans += word1[i++];
-            ans += word2[j++];
+        int i = 0;
+        while (i < word1.size() && i < word2.size()) {
+            ans += word1[i];
+            ans += word2[i];
+            i++;
         }
 
-        while (i < word1.size()) {
-            ans += word1[i++];
-        }
-
-        while (j < word2.size()) {
-            ans += word2[j++];
-        }
+        ans += word1.substr(i);
+        ans += word2.substr(i);
 
         return ans;
     }
