71b334ceeacb956dc4b8258ae87040fbc6ffa8dd
Avinash Kumar <avinashrk2107@gmail.com>
2026-09-03 12:04:21 +0530

    Time: 11 ms (31.3%), Space: 14.3 MB (52.62%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             int count = 0;
             for(int j = 0; j < nums.size(); j++){
-                if(nums[j] < nums[i]){
+                if(nums[j] < nums[i] && i != j){
                     count++;
                 }
             }
