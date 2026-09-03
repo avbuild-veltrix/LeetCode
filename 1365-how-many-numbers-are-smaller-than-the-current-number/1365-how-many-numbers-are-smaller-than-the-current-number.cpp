fa2f86e5bcd0bafb0226da70536c79ec965fb7ad
Avinash Kumar <avinashrk2107@gmail.com>
2026-09-03 12:21:02 +0530

    Time: 0 ms (100%), Space: 14.5 MB (20.72%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,15 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+//         vector<int> answer = {};
+//         for(int i = 0; i < nums.size(); i++){
+//             int count = 0;
+//             for(int j = 0; j < nums.size(); j++){
+//                 if(nums[j] < nums[i]){
+//                     count++;
+//                 }
+//             }
+//             answer.push_back(count);
+//         }
+//         return answer;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        vector<int> answer = {};
-        for(int i = 0; i < nums.size(); i++){
-            int count = 0;
-            for(int j = 0; j < nums.size(); j++){
-                if(nums[j] < nums[i] && i != j){
-                    count++;
-                }
+        vector<int> count(101, 0);
+        vector<int> answer;
+
+        for(int x : nums){
+            count[x]++;
+        }
+
+        for(int i = 1; i < 101; i++){
+            count[i] += count[i-1];
+        }
+
+        for(int x : nums){
+            if(x == 0){
+                answer.push_back(0);
+            }else{
+                answer.push_back(count[x-1]);
             }
-            answer.push_back(count);
         }
         return answer;
     }
