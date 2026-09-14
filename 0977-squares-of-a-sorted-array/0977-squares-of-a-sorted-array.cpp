d1286b9b210468064df1df60d6865198c9337403
Avinash Kumar <avinashrk2107@gmail.com>
2026-09-14 10:53:16 +0530

    Time: 3 ms (52.09%), Space: 33 MB (5.03%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,18 +1,77 @@
+//FIRST METHOD.
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i = 0; i < n; i++){
+//             nums[i] = nums[i] * nums[i];
+//         }
+//         sort(nums.begin(), nums.end());
+//         return nums;
+//     }
+// };
+
+// SECOND METHOD.
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         int n = nums.size();
+//         for(int i = 0; i < n; i++){
+//             nums[i] = nums[i] * nums[i];
+//         }
+//         for(int i = 1; i < n; i++){
+//             int j = i; 
+//             while(j > 0 && nums[j] < nums[j-1]){
+//                 swap(nums[j], nums[j-1]);
+//                 j--;
+//             }
+//         }
+//         return nums;
+//     }
+// };
+
+// THIRD METHOD.
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
+        vector<int> neg = {};
+        vector<int> pos = {};
+        vector<int> ans = {};
         for(int i = 0; i < n; i++){
-            nums[i] = nums[i] * nums[i];
-        }
-        // sort(nums.begin(), nums.end());
-        for(int i = 1; i < n; i++){
-            int j = i; 
-            while(j > 0 && nums[j] < nums[j-1]){
-                swap(nums[j], nums[j-1]);
-                j--;
+            if(nums[i] >= 0){
+                pos.push_back(nums[i]);
+            }else{
+                neg.push_back(nums[i]);
             }
         }
-        return nums;
+        for(int i = 0; i < neg.size(); i++){
+            neg[i] = neg[i]*neg[i];
+        }
+        reverse(neg.begin(), neg.end());
+        for(int i = 0; i < pos.size(); i++){
+            pos[i] = pos[i]*pos[i];
+        }
+        int i = 0, j = 0;
+        int a = neg.size();
+        int b = pos.size();
+        while(i < a && j < b){
+            if(neg[i] < pos[j]){
+                ans.push_back(neg[i]);
+                i++;
+            }else{
+                ans.push_back(pos[j]);
+                j++;
+            }
+        }
+        while(i < a){
+            ans.push_back(neg[i]);
+            i++;
+        }
+        while(j < b){
+            ans.push_back(pos[j]);
+            j++;
+        }
+        return ans;
     }
 };
